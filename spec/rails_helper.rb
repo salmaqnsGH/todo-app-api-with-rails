ec932cfe23f0059d3cd886b69008282a26eb8eaa
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-05-16 13:25:10 +0700

    fix spec tests for todo and item

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,21 +2,14 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
+require File.expand_path('../config/environment', __dir__)
+
 # require database cleaner at the top level
 require 'database_cleaner'
-
-require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
 
 # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
 Shoulda::Matchers.configure do |config|
@@ -26,11 +19,18 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-  config.infer_spec_type_from_file_location!
-  config.filter_rails_from_backtrace!
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.use_transactional_fixtures = true
+  # config.infer_spec_type_from_file_location!
+  # config.filter_rails_from_backtrace!
 
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
