0c040d640328b1f28b90c76dfc7a8428462c89e5
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-05-16 14:05:58 +0700

    created api for todos and items, and implemented test using rspec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
-end
+    include Response
+    include ExceptionHandler
+  end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,12 +26,15 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
   # config.use_transactional_fixtures = true
   # config.infer_spec_type_from_file_location!
   # config.filter_rails_from_backtrace!
 
+  config.include RequestSpecHelper, type: :request
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
 
