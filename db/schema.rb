9c4ad3436df46b4f3b063d49da8996574734063b
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-05-16 14:18:37 +0700

    installed bcrypt and created user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_16_044808) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_071455) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.boolean "done"
@@ -25,6 +25,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_044808) do
     t.string "created_by"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "items", "todos"
